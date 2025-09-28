081a62e2db3a00030b87ac4dff4eebdde0883078
Vikrant Yadav <aryan4763.0@gmail.com>
2025-09-28 21:39:05 +0530

    Update 04_magnitude_analysis.sql

--- scripts/04_magnitude_analysis.sql
+++ scripts/04_magnitude_analysis.sql
@@ -2,6 +2,10 @@
 ===============================================================================
 Magnitude Analysis
 ===============================================================================
+Purpose:
+    - To quantify data and group results by specific dimensions.
+    - For understanding data distribution across categories.
+===============================================================================
 */
 
 -- Find total customers by countries
